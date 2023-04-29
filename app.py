cc3e46cca74259edaf00398bd33662bba67d0d5e
Aadhithyan <62047164+Aadhithyanarayanan@users.noreply.github.com>
2023-04-29 18:57:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
-import os
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 import cv2
 import tensorflow as tf
 import numpy as np
 from flask_cors import CORS
-import json
-import pandas as pd
-import re
-import string
-import pickle
-import bz2file as bz2
 
 model = tf.keras.models.load_model('imageclassifier.h5')
 
-mp = bz2.BZ2File('model.pbz2', 'rb')
-mp = pickle.load(mp)
-td = bz2.BZ2File('vectorizer.pbz2', 'rb')
-td = pickle.load(td)
-
-
+UPLOAD_FOLDER = "static"
 ALLOWED_EXTENSION = set(['jpeg','jpg','png'])
 
 def allowed_file(filename):
@@ -28,48 +16,8 @@ def allowed_file(filename):
         return 0
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSION
 
-def  clean_text(text):
-    text = text.lower()
-    text = re.sub(r"i'm", "i am", text)
-    text = re.sub(r"\r", "", text)
-    text = re.sub(r"he's", "he is", text)
-    text = re.sub(r"she's", "she is", text)
-    text = re.sub(r"it's", "it is", text)
-    text = re.sub(r"that's", "that is", text)
-    text = re.sub(r"what's", "that is", text)
-    text = re.sub(r"where's", "where is", text)
-    text = re.sub(r"how's", "how is", text)
-    text = re.sub(r"\'ll", " will", text)
-    text = re.sub(r"\'ve", " have", text)
-    text = re.sub(r"\'re", " are", text)
-    text = re.sub(r"\'d", " would", text)
-    text = re.sub(r"\'re", " are", text)
-    text = re.sub(r"won't", "will not", text)
-    text = re.sub(r"can't", "cannot", text)
-    text = re.sub(r"n't", " not", text)
-    text = re.sub(r"n'", "ng", text)
-    text = re.sub(r"'bout", "about", text)
-    text = re.sub(r"'til", "until", text)
-    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
-    text = text.translate(str.maketrans('', '', string.punctuation)) 
-    text = re.sub("(\\W)"," ",text) 
-    text = re.sub('\S*\d\S*\s*','', text)
-    return text
-
-def make_test_predictions(df):
-    df.comment_text = df.comment_text.apply(clean_text)
-    X_test = df.comment_text
-    X_test_transformed = td.transform(X_test)
-    y_test_pred = mp.predict_proba(X_test_transformed)
-    result =  sum(y_test_pred[0])
-    if result >=1 :
-       return 1
-    else :
-       return 0
-    
 app = Flask(__name__)
 CORS(app)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/media/image', methods=['POST'])
 def upload_media():
@@ -99,19 +47,6 @@ def upload_media():
             return jsonify({'msg':1})
         else:
             return jsonify({'msg':0})
-
-@app.route("/media/text", methods=['POST'])
-def sanitize(): 
-    val = request.get_json()
-    val = val['comment']
-    comment_text = val
-    comment ={'comment_text':[comment_text]}
-    comment = pd.DataFrame(comment)
-    result = make_test_predictions(comment)
-    if(result==0):
-        return(jsonify({"msg": 1}))
-    else:
-        return(jsonify({"msg": 0}))
-
+        
 if __name__ == '__main__':
-    app.run(debug=True, port=os.environ.get('PORT', 5000))
+    app.run(debug=True, port=5000)
